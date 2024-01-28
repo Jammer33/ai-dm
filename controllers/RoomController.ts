db44d36aa786f0a2a66d9da81a34ce730f556d1d
Nihal Potdar <Nihalpot2002@gmail.com>
2024-01-28 18:07:35 -0500

    added game name and description

--- controllers/RoomController.ts
+++ controllers/RoomController.ts
@@ -4,14 +4,14 @@ import RoomQueries from '../queries/RoomQueries';
 class RoomController {
     // @createRoom creates a new room for the given user with this user token
     // returns a promise with the new session token
-    async createRoom(userToken: string) : Promise<string> {
+    async createRoom(userToken: string, name: string, description: string) : Promise<string> {
         let playerId = await this.findPlayerIdByUserToken(userToken);
         if(!playerId) {
             console.log("Could not create a new room for sessionToken ");
             return "";
         }
 
-        let sessionToken = await RoomQueries.createRoom(playerId);
+        let sessionToken = await RoomQueries.createRoom(playerId, name, description);
         return sessionToken;
     }
 
