d4fdf5dbb2636d9062fa7302f8a84b3300898509
Vansh Mago <vanshmago9@gmail.com>
2024-03-12 04:33:13 -0400

    (add) google sign in

--- routes/UserRoutes.ts
+++ routes/UserRoutes.ts
@@ -1,67 +1,95 @@
-import express from 'express';
-import UserController from '../controllers/UserController';
+import express from "express";
+import UserController from "../controllers/UserController";
 
 const router = express.Router();
 
 // signup user
-router.post('/signup', async (req, res) => {
-    var { username, password, email } = req.body;
-    const jwtToken = await UserController.signupUser({username, password, email});
-    res.cookie('token', jwtToken, {
-        httpOnly: false,
-        secure: true, // Ensure you're running your server with HTTPS for this to work
-        sameSite: 'lax',
-        expires: new Date(Date.now() + 60 * 60 * 1000 * 24), // 1 day
-        domain: process.env.NODE_ENV === 'dev' ? 'localhost' : 'wizardgm.ai'
-    });
-    return res.json({ message: "Successfully Signed Up" });
+router.post("/signup", async (req, res) => {
+  var { username, password, email } = req.body;
+  const jwtToken = await UserController.signupUser({
+    username,
+    password,
+    email,
+  });
+  res.cookie("token", jwtToken, {
+    httpOnly: false,
+    secure: true, // Ensure you're running your server with HTTPS for this to work
+    sameSite: "lax",
+    expires: new Date(Date.now() + 60 * 60 * 1000 * 24), // 1 day
+    domain: process.env.NODE_ENV === "dev" ? "localhost" : "wizardgm.ai",
+  });
+  return res.json({ message: "Successfully Signed Up" });
 });
 
 // verify user
-router.get('/verify', async (req: any, res) => {
-    if (!(req.cookies && req.cookies.token)) {
-        res.status(400);
-        return res.json({ message: "Verification Failed"});
-    }
-    const {email, userToken} = await UserController.verifyUser(req.cookies.token);
+router.get("/verify", async (req: any, res) => {
+  if (!(req.cookies && req.cookies.token)) {
+    res.status(400);
+    return res.json({ message: "Verification Failed" });
+  }
+  const { email, userToken } = await UserController.verifyUser(
+    req.cookies.token
+  );
 
-    return res.json({ message: "Successfully Verified", email: email, userToken: userToken });
+  return res.json({
+    message: "Successfully Verified",
+    email: email,
+    userToken: userToken,
+  });
 });
 
 // login user
-router.post('/login', async (req, res) => {
-    console.log(req.cookies);
-    var { email, password } = req.body;
-    const response = await UserController.loginUser({email, password});
-    res.cookie('token', response.jwtoken, {
-        httpOnly: false,
-        secure: true, // Ensure you're running your server with HTTPS for this to work
-        sameSite: 'lax',
-        expires: new Date(Date.now() + 60 * 60 * 1000 * 24), // 1 day
-        domain: process.env.NODE_ENV === 'dev' ? 'localhost' : 'wizardgm.ai'
-    });
-    return res.json({ message: "Successfully Logged In", userToken: response.userToken });
+router.post("/login", async (req, res) => {
+  console.log(req.cookies);
+  var { email, password } = req.body;
+  const response = await UserController.loginUser({ email, password });
+  res.cookie("token", response.jwtoken, {
+    httpOnly: false,
+    secure: true, // Ensure you're running your server with HTTPS for this to work
+    sameSite: "lax",
+    expires: new Date(Date.now() + 60 * 60 * 1000 * 24), // 1 day
+    domain: process.env.NODE_ENV === "dev" ? "localhost" : "wizardgm.ai",
+  });
+  return res.json({
+    message: "Successfully Logged In",
+    userToken: response.userToken,
+  });
+});
+
+// login user using Googe OAuth
+router.post("/googleAuthLogin", async (req, res) => {
+  console.log(req.cookies);
+  var { email, password } = req.body;
+  const response = await UserController.loginGoogleAuthUser({
+    email,
+    password,
+  });
+
+  return res.json({
+    message: "Successfully Logged In",
+    userToken: response.userToken,
+  });
 });
 
 // logout user
-router.post('/logout', async (req, res) => {
-    res.clearCookie('token');
-    return res.json({ message: "Successfully Logged Out" });
+router.post("/logout", async (req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "Successfully Logged Out" });
 });
 
 // forgot password
-router.post('/forgot-password', async (req, res) => {
-    var { email } = req.body;
-    await UserController.forgotPassword(email);
-    return res.json({ message: "Successfully Sent Email" });
+router.post("/forgot-password", async (req, res) => {
+  var { email } = req.body;
+  await UserController.forgotPassword(email);
+  return res.json({ message: "Successfully Sent Email" });
 });
 
 // reset password
-router.post('/reset-password', async (req, res) => {
-    var { password, resetToken } = req.body;
+router.post("/reset-password", async (req, res) => {
+  var { password, resetToken } = req.body;
 
-    await UserController.resetPasswordWithToken(password, resetToken);
-    return res.json({ message: "Password Changed" });
+  await UserController.resetPasswordWithToken(password, resetToken);
+  return res.json({ message: "Password Changed" });
 });
 
 export default router;
--- services/UserService.ts
+++ services/UserService.ts
@@ -1,140 +1,194 @@
 import { NewUser, UserLoginRequest } from "../models/General";
 import bcrypt from "bcrypt";
 import { generateResetToken, generateUserToken } from "../utils/UserUtils";
-import { BadRequestError, InternalServerError, UnauthorizedError } from "../middleware/ErrorHandler";
+import {
+  BadRequestError,
+  InternalServerError,
+  UnauthorizedError,
+} from "../middleware/ErrorHandler";
 import jwt from "jsonwebtoken";
 import UserQueries from "../queries/UserQueries";
-import AWS from "aws-sdk"
+import AWS from "aws-sdk";
 
 const RESET_TOKEN_TABLE = "dev_reset_token";
 const RESET_TOKEN_EXPIRATION = 60 * 60 * 2; // 2 hours
 
 class UserService {
-    docClient = new AWS.DynamoDB.DocumentClient();
+  docClient = new AWS.DynamoDB.DocumentClient();
 
-    async signupUser(user: NewUser): Promise<string> {
-        // generate a random token with full alphabet and numbers not just hex no symbols
-        const userToken = generateUserToken();
+  async signupUser(user: NewUser): Promise<string> {
+    // generate a random token with full alphabet and numbers not just hex no symbols
+    const userToken = generateUserToken();
 
-        // hash and salt the password
-        const hashedPassword = bcrypt.hashSync(user.password, 10);
-        // store the user in the database
+    // hash and salt the password
+    const hashedPassword = bcrypt.hashSync(user.password, 10);
+    // store the user in the database
 
-        const createdUser = await UserQueries.create(user.username, user.email, hashedPassword, userToken);
+    const createdUser = await UserQueries.create(
+      user.username,
+      user.email,
+      hashedPassword,
+      userToken
+    );
 
-        if (!createdUser) {
-            throw new InternalServerError("Error creating user");
-        }
-        const jwtoken = jwt.sign({ userToken }, process.env.SECRET_KEY!!, { expiresIn: '1w' })
+    if (!createdUser) {
+      throw new InternalServerError("Error creating user");
+    }
+    const jwtoken = jwt.sign({ userToken }, process.env.SECRET_KEY!!, {
+      expiresIn: "1w",
+    });
 
-        return await jwtoken;
+    return await jwtoken;
+  }
+
+  async verifyUser(token: string): Promise<{ email: string; userToken: any }> {
+    // retrieve the user from the database
+    const payload = jwt.verify(token, process.env.SECRET_KEY!!) as any;
+    console.log(payload);
+    console.log(typeof payload);
+    let email: string = "";
+
+    if (payload.userToken) {
+      const storedUser = await UserQueries.findByToken(payload.userToken);
+      console.log("|||");
+      console.log(storedUser);
+      if (!storedUser) {
+        throw new BadRequestError("No user found with that token");
+      }
+      email = storedUser.email;
     }
 
-    async verifyUser(token: string): Promise<{ email: string; userToken: any; }> {
-        // retrieve the user from the database
-        const payload = jwt.verify( token, process.env.SECRET_KEY!! ) as any;
-        console.log(payload);
-        console.log(typeof payload);
-        let email: string =  '';
+    return { email, userToken: payload.userToken };
+  }
 
-        if (payload.userToken) {
-            const storedUser = await UserQueries.findByToken(payload.userToken);
-            console.log("|||")
-            console.log(storedUser);
-            if (!storedUser) {
-                throw new BadRequestError("No user found with that token");
-            }
-            email = storedUser.email;
-        }
-
-        return {email, userToken: payload.userToken};
+  async loginUser(
+    user: UserLoginRequest
+  ): Promise<{ jwtoken: string; userToken: string }> {
+    // retrieve the user from the database
+    const storedUser = await UserQueries.findByEmail(user.email);
+    if (!storedUser) {
+      throw new BadRequestError("No user found with that email");
+    }
+    // compare the passwords
+    const match = bcrypt.compareSync(user.password, storedUser.password);
+    if (!match) {
+      throw new UnauthorizedError("Password and email do not match");
     }
 
-    async loginUser(user: UserLoginRequest): Promise<{ jwtoken: string; userToken: string; }> {
-        // retrieve the user from the database
-        const storedUser = await UserQueries.findByEmail(user.email);
-        if (!storedUser) {
-            throw new BadRequestError("No user found with that email");
-        }
-        // compare the passwords
-        const match = bcrypt.compareSync(user.password, storedUser.password);
-        if (!match) {
-            throw new UnauthorizedError("Password and email do not match");
-        }
+    const jwtoken = jwt.sign(
+      { userToken: storedUser.userToken },
+      process.env.SECRET_KEY!!,
+      { expiresIn: "1w" }
+    );
 
-        const jwtoken = jwt.sign({ userToken: storedUser.userToken }, process.env.SECRET_KEY!!, { expiresIn: '1w' })
+    return { jwtoken, userToken: storedUser.userToken };
+  }
 
-        return {jwtoken, userToken: storedUser.userToken};
+  async loginGoogleAuthUser(
+    user: UserLoginRequest
+  ): Promise<{ userToken: string }> {
+    // retrieve the user from the database
+    const storedUser = await UserQueries.findByEmail(user.email);
+    var userToken = "";
+
+    if (!storedUser) {
+      // create a new user if player has signed on using google auth for the first time
+      userToken = generateUserToken();
+
+      // only story usertoken and email
+      const [username, password] = ["", ""];
+
+      const createdUser = await UserQueries.create(
+        username,
+        user.email,
+        password,
+        userToken
+      );
+
+      if (!createdUser) {
+        throw new InternalServerError("Error creating user");
+      }
     }
 
-    async resetPassword(oldPassword: string, newPassword: string, userToken: string): Promise<void> {
-        // retrieve the user from the database
-        const storedUser = await UserQueries.findByToken(userToken);
-        if (!storedUser) {
-            throw new BadRequestError("No user found with that token");
-        }
+    return { userToken: storedUser ? storedUser.userToken : userToken };
+  }
 
-        // compare the passwords
-        const match = bcrypt.compareSync(oldPassword, storedUser.password);
-        if (!match) {
-            throw new UnauthorizedError("Password and email do not match");
-        }
-
-        const hashedPassword = bcrypt.hashSync(newPassword, 10);
-
-        await UserQueries.updatePassword(hashedPassword, userToken);
+  async resetPassword(
+    oldPassword: string,
+    newPassword: string,
+    userToken: string
+  ): Promise<void> {
+    // retrieve the user from the database
+    const storedUser = await UserQueries.findByToken(userToken);
+    if (!storedUser) {
+      throw new BadRequestError("No user found with that token");
     }
 
-    async createResetToken(email: string): Promise<string> {
-        const user = await UserQueries.findByEmail(email);
-        if (!user) {
-            throw new BadRequestError("No user found with that email");
-        }
+    // compare the passwords
+    const match = bcrypt.compareSync(oldPassword, storedUser.password);
+    if (!match) {
+      throw new UnauthorizedError("Password and email do not match");
+    }
 
-        const resetToken = generateResetToken();
+    const hashedPassword = bcrypt.hashSync(newPassword, 10);
 
-        await this.docClient.put({
-            TableName: RESET_TOKEN_TABLE,
-            Item: {
-                token: resetToken,
-                UserToken: user.userToken,
-                ExpirationTime: Math.floor(Date.now() / 1000) + RESET_TOKEN_EXPIRATION,
-            },
+    await UserQueries.updatePassword(hashedPassword, userToken);
+  }
+
+  async createResetToken(email: string): Promise<string> {
+    const user = await UserQueries.findByEmail(email);
+    if (!user) {
+      throw new BadRequestError("No user found with that email");
+    }
+
+    const resetToken = generateResetToken();
+
+    await this.docClient.put(
+      {
+        TableName: RESET_TOKEN_TABLE,
+        Item: {
+          token: resetToken,
+          UserToken: user.userToken,
+          ExpirationTime:
+            Math.floor(Date.now() / 1000) + RESET_TOKEN_EXPIRATION,
         },
-            (err) => {
-                if (err) {
-                    console.log("Error", err);
-                }
-            }
-        
-        );
-
-        return resetToken;
-    }
-
-    async resetPasswordWithToken(newPassword: string, resetToken: string): Promise<void> {
-        const params = {
-            TableName: RESET_TOKEN_TABLE,
-            Key: {
-                token: resetToken,
-            },
-        };
-
-        const data = await this.docClient.get(params).promise();
-        if (!data.Item) {
-            throw new BadRequestError("Invalid reset token");
+      },
+      (err) => {
+        if (err) {
+          console.log("Error", err);
         }
+      }
+    );
 
-        await this.docClient.delete(params, (err) => {
-            if (err) {
-                console.log("Error deleting reset key", err);
-            }
-        });
+    return resetToken;
+  }
 
-        const hashedPassword = bcrypt.hashSync(newPassword, 10);
+  async resetPasswordWithToken(
+    newPassword: string,
+    resetToken: string
+  ): Promise<void> {
+    const params = {
+      TableName: RESET_TOKEN_TABLE,
+      Key: {
+        token: resetToken,
+      },
+    };
 
-        await UserQueries.updatePassword(hashedPassword, data.Item.UserToken);
+    const data = await this.docClient.get(params).promise();
+    if (!data.Item) {
+      throw new BadRequestError("Invalid reset token");
     }
+
+    await this.docClient.delete(params, (err) => {
+      if (err) {
+        console.log("Error deleting reset key", err);
+      }
+    });
+
+    const hashedPassword = bcrypt.hashSync(newPassword, 10);
+
+    await UserQueries.updatePassword(hashedPassword, data.Item.UserToken);
+  }
 }
 
 export default new UserService();
